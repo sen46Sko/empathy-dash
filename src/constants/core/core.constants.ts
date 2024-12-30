57ee9556dd370ab085e2b4e64cfef21beab57f5f
sen46Sko <visosensey@gmail.com>
2024-12-30 11:28:08 +0200

    deploy message;

--- src/constants/core/core.constants.ts
+++ src/constants/core/core.constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URI = 'http://localhost:3000';
+export const BASE_URI = 'https://empathy-dashboard-nest.vercel.app';
 export const NOTES_BASE_URI = 'https://di16hiobp3qrl.cloudfront.net';
